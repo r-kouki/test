10e205d75183c2f008cfb1a2fe881e10687d3c17
kouki abderrahmen <abderrahmen.kouki@polytechnicien.tn>
2024-12-30 04:51:48 +0100

    Fix: Update project image imports

---
+++ src/data/projectsData.ts
@@ -0,0 +1,36 @@
+import dsdeclarationImg from '../assets/images/dsdeclaration.webp'
+import dspayeImg from '../assets/images/dspaye.webp'
+import dscomptaImg from '../assets/images/dscompta.webp'
+
+export const projectsData = [
+  {
+    id: 1,
+    title: 'DS DECLARATION',
+    description: {
+      en: 'Your description in English',
+      fr: 'Your description in French'
+    },
+    image: dsdeclarationImg,
+    link: 'https://declaration.kouki-softwares.com'
+  },
+  {
+    id: 2,
+    title: 'DS PAYE',
+    description: {
+      en: 'Your description in English',
+      fr: 'Your description in French'
+    },
+    image: dspayeImg,
+    link: 'https://paye.kouki-softwares.com'
+  },
+  {
+    id: 3,
+    title: 'DS COMPTA',
+    description: {
+      en: 'Your description in English',
+      fr: 'Your description in French'
+    },
+    image: dscomptaImg,
+    link: 'https://compta.kouki-softwares.com'
+  }
+] 
